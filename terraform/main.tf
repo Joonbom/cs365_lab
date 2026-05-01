1e529e917add7f0f64629d8a2443224d97b3051f
Joonbom <wachirakorn.chi@gmail.com>
2026-05-01 09:12:45 +0000

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -108,7 +108,7 @@ resource "aws_ecr_repository" "frontend_repo" {
 
 # Create ECR Repository for Backend
 resource "aws_ecr_repository" "backend_repo" {
-  name                 = "backend-repo"
+  name                 = "backend-repo-v2"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
