15ca0960556c19349515b48be2472bb5bb36fe20
Joonbom <wachirakorn.chi@gmail.com>
2026-05-01 08:54:41 +0000

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,17 +60,49 @@ resource "aws_security_group" "login_page_sg" {
   }
 }
 
+# Generate a Private Key
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+# Create an AWS Key Pair
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "login_page_key"
+  public_key = tls_private_key.my_key.public_key_openssh
+}
+
+# Save the private key locally
+resource "local_file" "private_key" {
+  content         = tls_private_key.my_key.private_key_pem
+  filename        = "${path.module}/private_key.pem"
+  file_permission = "0600"
+}
+
+# Create ECR Repository
+resource "aws_ecr_repository" "login_page_repo" {
+  name                 = "login-page-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 # EC2 Instance
 resource "aws_instance" "login_server" {
   ami           = data.aws_ami.amazon_linux_2023.id
   instance_type = "t2.micro" 
   subnet_id     = data.aws_subnets.default.ids[0]
   
+  # Attach the LabInstanceProfile to allow ECR access
+  iam_instance_profile = "LabInstanceProfile"
+
   # Ensure we attach the security group
   vpc_security_group_ids = [aws_security_group.login_page_sg.id]
 
-  # vockey is the default standard key pair automatically provided in AWS Learner Labs
-  key_name = "vockey" 
+  # Use the dynamically generated key pair
+  key_name = aws_key_pair.my_key_pair.key_name 
 
   # Startup script to install Docker
   user_data = <<-EOF
@@ -95,4 +127,9 @@ output "public_ip" {
 output "website_url" {
   value       = "http://${aws_instance.login_server.public_ip}"
   description = "The URL to access the login page"
+}
+
+output "ecr_repository_url" {
+  value       = aws_ecr_repository.login_page_repo.repository_url
+  description = "The URL of the ECR repository"
 }
