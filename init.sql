0824bf9217dd9a3bb3e7711964ae2c1323321fa2
Joonbom <wachirakorn.chi@gmail.com>
2026-05-01 09:47:16 +0000

    fix

--- init.sql
+++ init.sql
@@ -4,5 +4,5 @@ CREATE TABLE users (
     password_hash VARCHAR(255) NOT NULL
 );
 
--- Test user with username: testuser, password: password123
-INSERT INTO users (username, password_hash) VALUES ('testuser', '$2b$10$EP36v2i4hIuV0v4/9R50cOH7kC8I3r2yN2F.zZ9Xj0q.tN1C2iQp6');
+-- Test user with username: testuser@gmail.com, password: password123
+INSERT INTO users (username, password_hash) VALUES ('testuser@gmail.com', '$2b$10$BfKAwDVE6Pw7Vjd9uuMqnOYwn4Zg8VL5tl49qs/eLa5/mfzsioTm.');
